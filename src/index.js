3ebf96ef597f6bd1023e3c43b5051fc5d11053c9
Matija301 <115715345+Matija301@users.noreply.github.com>
2023-02-06 10:48:42 +0100

    Fixed bug with button that has fixed position

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-const btnOpen = document.querySelector (".menu-open")
-const btnClosed = document.querySelector (".menu-closed")
-const btn= document.querySelector(".header-button")
-const headerTex = document.querySelector(".header-tex")
+const btnOpen = document.querySelector(".menu-open");
+const btnClosed = document.querySelector(".menu-closed");
+const btn = document.querySelector(".header-button");
+const headerTex = document.querySelector(".header-tex");
 
-btn.addEventListener("click",(e) =>{
-    btnOpen.classList.toggle("hidden")
-    btnClosed.classList.toggle("hidden")
-    btnOpen.classList.toggle("sticky")
-    headerTex.classList.toggle("header-tex-open")
-})
+btn.addEventListener("click", () => {
+  btnOpen.classList.toggle("hidden");
+  btnClosed.classList.toggle("hidden");
+  btnOpen.classList.toggle("sticky");
+  headerTex.classList.toggle("header-tex-open");
+});
